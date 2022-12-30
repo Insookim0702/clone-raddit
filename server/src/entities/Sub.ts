6a5d885aa99380f9ebe005d6850b690b3bba2a5e
glorangJohn <42515875+Insookim0702@users.noreply.github.com>
2022-12-30 10:06:12 +0900

    Entity 작성 중

---
+++ server/src/entities/Sub.ts
@@ -0,0 +1,29 @@
+import { BaseEntity, Column, Index } from "typeorm";
+import User from "./User";
+
+export default class Sub extends BaseEntity {
+  @Index()
+  @Column({ unique: true })
+  name: string;
+
+  @Column()
+  title: string;
+
+  @Column({ type: "text", nullable: true })
+  description: string;
+
+  @Column({ nullable: true })
+  imageUrn: string;
+
+  @Column({ nullable: true })
+  bannerUrn: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  user: User;
+
+  @Column()
+  posts: Post[];
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
-import exrpess from "express";
+import express from "express";
 import morgan from "morgan";
-
-const app = exrpess();
-app.use(exrpess.json());
+import { AppDataSource } from "./data-source";
+const app = express();
+app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (_, res) => res.send("running"));
@@ -11,4 +11,9 @@ let port = 5000;
 
 app.listen(port, async () => {
   console.log(`server running at http://localhost:${port}`);
+  AppDataSource.initialize()
+    .then(() => {
+      console.log("database initialize");
+    })
+    .catch((error) => console.log(error));
 });
